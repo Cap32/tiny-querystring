12e4bd4699ceaf4518c1f429c3e9e3c8661254f2
Webb Peng <webb.png@gmail.com>
2017-09-09 01:53:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tiny-querystring
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+exports.parse = function parse(str) {
+	return (str + '')
+		.replace(/\+/g, ' ')
+		.split('&')
+		.filter(function (item) {
+			return !/^\s*$/.test(item);
+		})
+		.reduce(function (obj, item, index) {
+			var ref = item.split('=');
+			var key = ref[0] || '';
+			var val = ref[1] || '';
+			var prev = obj[key];
+			obj[key] = typeof prev === 'undefined' ? val : [].concat(prev, val);
+			return obj;
+		}, {})
+	;
+};
+
+exports.stringify = function stringify(obj) {
+	return Object.keys(obj || {})
+		.reduce(function (arr, key) {
+			var val = obj[key];
+			if (val instanceof Array) { val = val.join('&' + key + '='); }
+			arr.push(key + '=' + val);
+			return arr;
+		}, [])
+		.join('&')
+		.replace(/\s/g, '+')
+	;
+};
